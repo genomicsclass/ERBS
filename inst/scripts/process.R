ed88903f4179d42d2c216289aaf8f6e4a108fb06
Mike Love <michaelisaiahlove@gmail.com>
2015-04-01 09:58:02 -0400

    adding metadata

--- inst/scripts/process.R
+++ inst/scripts/process.R
@@ -5,5 +5,7 @@ for (i in 1:2) {
   names(mcols(x)) <- c("name","score","col","signalValue","pValue","qValue","peak")
   assign(sampleInfo[i,2], x)
 }
+metadata(HepG2) <- list("ENCODE accession: ENCSR000EEW. ESRRA ChIP-seq peaks of HepG2 cell line https://www.encodeproject.org/experiments/ENCSR000EEW/")
+metadata(GM12878) <- list("ENCODE accession: ENCSR000DYQ. ESRRA ChIP-seq peaks of GM12878 cell line https://www.encodeproject.org/experiments/ENCSR000DYQ/")
 save(HepG2, file="../../data/HepG2.rda")
 save(GM12878, file="../../data/GM12878.rda")
